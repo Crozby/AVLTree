682579aa7c86a04a4852b23ebd98337215714adf
Crozby <deadwhisper1992@gmail.com>
2018-08-17 13:12:15 +0300

    AVLTree

---
+++ AVLTree/AVLTree.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTree
+{
+    [Serializable]
+    internal sealed class AVLTree
+    {
+        private Node root;
+
+        internal int GetHeight()
+        {
+            return Node.GetHeight(root);
+        }
+
+        public bool Search(int key)
+        {
+            return Search(root, key);
+        }
+
+        private static bool Search(Node node, int key)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (key > node.key)
+            {
+                return Search(node.Right, key);
+            }
+            else if (key < node.key)
+            {
+                return Search(node.Left, key);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Insert(int key)
+        {
+            if (root == null)
+            {
+                root = new Node(key);
+                return;
+            }
+            root = Insert(root, key);
+        }
+
+        public void Remove(int key)
+        {
+            root = Remove(root, key);
+        }
+
+        private static Node Remove(Node node, int key)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            else if (key > node.key)
+            {
+                node.Right = Remove(node.Right, key);
+            }
+            else if (key < node.key)
+            {
+                node.Left = Remove(node.Left, key);
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    return node.Left;
+                }
+                Node min = node.Right.FindMin();
+                min.Right = node.Right.RemoveMin();
+                min.Left = node.Left;
+                return DoBalance(min);
+            }
+            return DoBalance(node);
+        }
+
+        private static Node Insert(Node node, int key)
+        {
+            if (node == null)
+            {
+                return new Node(key);
+            }
+            if (key >= node.key)
+            {
+                node.Right = Insert(node.Right, key);
+            }
+            else
+            {
+                node.Left = Insert(node.Left, key);
+            }
+            return DoBalance(node);
+        }
+
+        private static Node DoBalance(Node node)
+        {
+            node.FixHeight();
+            if (node.BalanceFactor() == 2)
+            {
+                if (node.Right.BalanceFactor() < 0)
+                {
+                    node.Right = node.Right.RightRotate();
+                }
+                return node.LeftRotate();
+            }
+            if (node.BalanceFactor() == -2)
+            {
+                if (node.Left.BalanceFactor() > 0)
+                {
+                    node.Left = node.Left.LeftRotate();
+                }
+                return node.RightRotate();
+            }
+            return node;
+        }
+
+        [Serializable]
+        private class Node
+        {
+            public readonly int key;
+            private sbyte height;
+            public Node Left;
+            public Node Right;
+
+            public Node(int key)
+            {
+                this.key = key;
+                height = 1;
+            }
+
+            public int BalanceFactor()
+            {
+                return GetHeight(Right) - GetHeight(Left);
+            }
+
+            public void FixHeight()
+            {
+                sbyte hl = GetHeight(Left);
+                sbyte hr = GetHeight(Right);
+                height = (sbyte)((hl > hr ? hl : hr) + 1);
+            }
+
+            public Node FindMin()
+            {
+                return Left != null ? Left.FindMin() : this;
+            }
+
+            public Node RemoveMin()
+            {
+                if (Left == null)
+                {
+                    return Right;
+                }
+                Left = Left.RemoveMin();
+                return DoBalance(this);
+            }
+
+            public Node RightRotate()
+            {
+                Node q = Left;
+                Left = q.Right;
+                q.Right = this;
+                this.FixHeight();
+                q.FixHeight();
+                return q;
+            }
+
+            public Node LeftRotate()
+            {
+                Node p = Right;
+                Right = p.Left;
+                p.Left = this;
+                this.FixHeight();
+                p.FixHeight();
+                return p;
+            }
+
+            public static sbyte GetHeight(Node node)
+            {
+                return node != null ? node.height : (sbyte)0;
+            }
+        }
+    }
+}
---
+++ AVLTree/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using TimeTest;
+
+namespace AVLTree
+{
+    class Program
+    {
+        const int MaxCount = 10000;
+
+        static void RealMain()
+        {
+            int[] baseArray = InitialArray();
+            var tree = new AVLTree();
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            Action<int>[] delAVL = new Action<int>[] { tree.Insert, tree.Remove };
+            Console.WriteLine("AVL Tree test. Fill, Search, Remove and Serialize");
+            using (new OperationTimer("AVLTreeTest. Fill, Search, Remove and Serialize time test."))
+            {
+                for (int oper = 0; oper < 2; oper++)
+                {
+                    for (int i = 0; i < MaxCount; i++)
+                    {
+                        delAVL[oper](baseArray[i]);
+                    }
+                    if (oper == 0)
+                    {
+                        formatter.Serialize(stream, tree);
+                    }
+                    Console.WriteLine("Tree height: " + tree.GetHeight().ToString());
+                    Console.Write(oper == 0 ? $"item = {baseArray[500]}, Search item result = {tree.Search(baseArray[500])}" : "removing has completed");
+                    Console.WriteLine();
+                }
+            }
+            stream.Position = 0;
+            tree = (AVLTree)formatter.Deserialize(stream);
+            stream.Dispose();
+            Console.WriteLine("Deserializing the tree...\nDeserialized tree height: " + tree.GetHeight());
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            RealMain();
+        }
+
+        public static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            String resourceName = Assembly.GetExecutingAssembly().GetManifestResourceNames().FirstOrDefault(rn => 
+                    rn.EndsWith(new AssemblyName(args.Name).Name + ".dll"));
+            if (resourceName == null) return null;
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AVLTree.OperationTimer.dll"))
+            {
+                byte[] asssemlyData = new byte[stream.Length];
+                stream.Read(asssemlyData, 0, asssemlyData.Length);
+                return Assembly.Load(asssemlyData);
+            }
+        }
+
+        private static int[] InitialArray()
+        {
+            int[] array = new int[MaxCount];
+            Random rand = new Random();
+            for (int i = 0; i < MaxCount; i++)
+            {
+                array[i] = rand.Next();
+            }
+            return array;
+        }
+    }
+}
